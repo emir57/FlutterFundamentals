297add08eaf5167d21aaac146eb1fa55d1700add
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 20:04:04 +0300

    set note variable

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String titleMessage = "Hello world!";
+    int note = 65;
     return Scaffold(
       appBar: AppBar(
         title: Text(titleMessage),
