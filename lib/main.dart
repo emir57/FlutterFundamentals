94e782676e64f937586b3e8f40dfbe8104b605a2
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 17:39:56 +0300

    runApp(app)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
 void main() {
   String text = "Merhaba Emir";
   int dogumYili = 2002;
   double oran = 1.15;
   bool isSign = false;
+  // print(text);
+  // print("Doğum yılı: " + dogumYili.toString());
+  // print("Oran: " + oran.toString());
+  runApp(app);
 
-  print(text);
-  print("Doğum yılı: " + dogumYili.toString());
-  print("Oran: " + oran.toString());
 }
