ab57aa46a4a516d11c4bf9db678a98c679ab916c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 19:42:55 +0300

    main -> appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 void main() {
-  
+  String titleMessage = "Hello world!";
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(titleMessage),
+      ),
+    ),
+  ));
 }
