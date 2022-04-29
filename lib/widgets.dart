11a19f14eed076abbadf823c54281d98154b59fd
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 18:16:58 +0300

    refactoring -> widgets

---
+++ lib/widgets.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  String titleMessage = "Merhaba ilk uygulamam";
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(titleMessage),
+      ),
+      body: Center(
+        child: Text("Gövde kısmı"),
+      ),
+    ),
+  ));
+}
