d39313e6ba5298aedc137ee1d2c546d18f529715
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 18:15:28 +0300

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  String text = "Merhaba Emir";
-  int dogumYili = 2002;
-  double oran = 1.15;
-  bool isSign = false;
-  // print(text);
-  // print("Doğum yılı: " + dogumYili.toString());
-  // print("Oran: " + oran.toString());
   String titleMessage = "Merhaba ilk uygulamam";
   runApp(MaterialApp(
     home: Scaffold(
---
+++ lib/variables.dart
@@ -0,0 +1,9 @@
+void main() {
+  String text = "Merhaba Emir";
+  int dogumYili = 2002;
+  double oran = 1.15;
+  bool isSign = false;
+  print(text);
+  print("Doğum yılı: " + dogumYili.toString());
+  print("Oran: " + oran.toString());
+}
