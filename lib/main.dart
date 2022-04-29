f76d891a594f450d9b2e49ce3b86fe77b0a25816
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 17:10:31 +0300

    print string and double

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 void main() {
   String text = "Merhaba Emir";
   int dogumYili = 2002;
+  double oran = 1.15;
   print(text);
-  print(dogumYili);
+  print("Doğum yılı: " + dogumYili.toString());
 }
