86c0cd47d63a9ab9e66c6a6bab9f4866b980bf3e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-02 09:13:57 +0300

    do while

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,19 @@ void main() {
     // print(i);
   }
   var products = ["Laptop", "Mouse", "Keyboard"];
-  for(var i = 0;i<products.length;i++){
+  for (var i = 0; i < products.length; i++) {
     print(products[i]);
   }
-  for(var product in products){
+  for (var product in products) {
     print(product);
   }
   int sayi = 3;
-  while( sayi >0){
+  while (sayi > 0) {
     print(sayi);
     sayi--;
   }
+
+  do {
+    print("do while");
+  } while (false);
 }
