2a0211aa08867be3cbd59657b5bcdf1dbfc918dc
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-02 08:54:04 +0300

    lists

--- lib/lists.dart
+++ lib/lists.dart
@@ -1,3 +1,12 @@
 void main() {
-  
+  var products = [];
+  products.add("Laptop");
+  products.add("Mouse");
+  products.add("Keyboard");
+  products.add("Monitor");
+  products.add("Microphone");
+  print(products);
+
+  var cities = ["Ankara", "İstanbul", "İzmir"];
+  print(cities.where((element) => element.contains("r")));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  var products = [];
-  products.add("Laptop");
-  products.add("Mouse");
-  products.add("Keyboard");
-  products.add("Monitor");
-  products.add("Microphone");
-  print(products);
-
-  var cities = ["Ankara", "İstanbul", "İzmir"];
-  print(cities);
+  
 }
