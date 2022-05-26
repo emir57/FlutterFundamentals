c41e268da6dca40d17bc2c4ddbd04608e76ee620
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-26 19:28:24 +0300

    sayHello function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
-void main(){
-  
+void main() {
+  sayHello2("my_username");
+}
+
+void sayHello() {
+  print("Hello there!");
+}
+
+void sayHello2(String username) {
+  print("Hello, " + username);
 }
