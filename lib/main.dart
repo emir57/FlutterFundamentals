c99f302f880ab2766ac475e70417846a416a68c8
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-26 20:45:17 +0300

    test2 function

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 void main() {
   sayHello2("my_username");
   test1(1);
+  test2(num1: 2, num2: 3, num3: 4);
 }
 
 void sayHello() {
@@ -19,6 +20,12 @@ double calculate(double a, double percent) {
 }
 
 test1(int num1, [int? num2, int? num3]) {
+  print(num1);
+  print(num2);
+  print(num3);
+}
+
+test2({int? num1, int? num2, int? num3}) {
   print(num1);
   print(num2);
   print(num3);
